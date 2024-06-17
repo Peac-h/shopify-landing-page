9d95c3419482a2f76e0aca268a0b3da5706287f2
_Peach <p6eac.h@gmail.com>
2024-06-17 17:09:18 +0400

    Add metatags

--- src/features/IllustrationShopify.tsx
+++ src/features/IllustrationShopify.tsx
@@ -6,11 +6,11 @@ export const IllustrationShopify = (): JSX.Element => {
   return (
     <div>
       <div className="mx-auto aspect-square w-3/4 max-w-[464px] -translate-y-5 md:hidden md:w-1/3">
-        <video src={VideoSm} autoPlay loop className="h-full w-full" />
+        <video src={VideoSm} autoPlay loop muted className="h-full w-full" />
       </div>
 
       <div className="mx-auto hidden aspect-square w-3/4 max-w-[464px] -translate-y-7 md:block md:w-1/3">
-        <video src={VideoMd} autoPlay loop className="h-full w-full" />
+        <video src={VideoMd} autoPlay loop muted className="h-full w-full" />
       </div>
     </div>
   );
